ff558a44e8645a63c25fe0bfa5989b96fd9464bd
Medi Mubiangata <medi.mubiangata@student.ehb.be>
2022-10-06 08:19:21 +0100

    Excercise  Write your name

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,51 @@
 "use strict";
+
+drawName();
+
+function drawName() {
+
+let canvas = document.querySelector('canvas')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d')
+
+context.lineWidth = 10;
+context.strokeStyle = 'red';
+context.beginPath();
+context.moveTo(250, 200);
+context.lineTo(100, 100);
+context.lineTo(100, 100);
+context.lineTo(100, 500);
+context.moveTo(250,200);
+context.lineTo(400, 100);
+context.lineTo(400,500);
+context.stroke();
+
+context.beginPath();
+context.strokeStyle = 'black';
+context.moveTo(600, 300);
+context.lineTo(600, 100);
+context.lineTo(600, 500);
+context.moveTo(600, 100);
+context.lineTo(800, 100)
+context.moveTo(600, 250)
+context.lineTo(800,250);
+context.moveTo(600, 500)
+context.lineTo(800,500);
+context.stroke();
+
+context.beginPath();
+context.strokeStyle = 'green';
+context.moveTo(900, 100);
+context.lineTo(900, 500);
+context.lineTo(1100, 300);
+context.lineTo(900, 100);
+context.stroke();
+
+context.beginPath();
+context.strokeStyle = 'blue';
+context.moveTo(1300, 100);
+context.lineTo(1300, 500);
+context.stroke();
+
+}
